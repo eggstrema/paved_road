26c2345c36520ffe7e067485abbbcb192dd4969a
egga <egga.hartung@ionos.com>
2022-01-21 19:52:21 +0100

    add hello egga controller

---
+++ src/main/java/de/egga/paved_road/HelloController.java
@@ -0,0 +1,12 @@
+package de.egga.paved_road;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String index() {
+        return "hello egga";
+    }
+}
