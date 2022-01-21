e791e1b1868b7e242853120e283af9e86b09ec47
egga <egga.hartung@ionos.com>
2022-01-21 21:42:54 +0100

    add rest assured test

---
+++ src/test/java/de/egga/paved_road/reestAssured/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package de.egga.paved_road.reestAssured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import static io.restassured.RestAssured.given;
+import static net.javacrumbs.jsonunit.JsonMatchers.jsonEquals;
+
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Test
+    public void it_should_return_status_code() {
+        given().port(port)
+                .when().get("/hello")
+                .then().statusCode(200);
+    }
+
+    @Test
+    public void it_should_return_hello_object() throws Exception {
+        given().port(port)
+                .when().get("/hello/")
+                .then().body(jsonEquals("{'greeting': 'yo', 'addressee': 'egga'}"));
+
+    }
+}
