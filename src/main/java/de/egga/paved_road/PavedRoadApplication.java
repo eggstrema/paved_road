da4d842b7fb07259c0676c06ea8117a04ada89d3
Egga <57639719+eggstrema@users.noreply.github.com>
2022-01-21 19:19:25 +0100

    add spring boot starter

---
+++ src/main/java/de/egga/paved_road/PavedRoadApplication.java
@@ -0,0 +1,13 @@
+package de.egga.paved_road;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PavedRoadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PavedRoadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/egga/paved_road/PavedRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package de.egga.paved_road;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PavedRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
